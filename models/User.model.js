d157e7e77302f8e9a80e08712fbb56961fbe57cf
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-24 14:50:17 -0500

    Add profile page and route Add a route for rendering the profile page. Fix a typo in User.model.

--- models/User.model.js
+++ models/User.model.js
@@ -27,7 +27,7 @@ var UserSchema = new mongoose.Schema({
 // authenticate input against database documents
 UserSchema.statics.authenticate = function(email, password, callback) {
 	User.findOne({ email: email })
-		exec(function (error, user) {
+		.exec(function (error, user) {
 			if (error) {
 				return callback(error);
 			} else if ( !user ) {
